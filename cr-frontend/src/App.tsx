327f1afdc97886c9511b57363bd8a2629798732c
N'Mamacupmusap <43214299+Mamacupmusap@users.noreply.github.com>
2020-09-06 12:58:08 +0700

    add new course form

--- cr-frontend/src/App.tsx
+++ cr-frontend/src/App.tsx
@@ -1,11 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import {Course} from './interfaces'
-import CourseItem from './CourseItem';
+import CourseItem from './components/CourseItem';
+import NewCourseForm from './components/NewCourseForm';
 
 const App =() =>{
   const [courses,setCourses] = useState<Course[]>([]);
-  
+  const [formVisible,setFormVisible]= useState<boolean>(false);
+
+  const toggleFormVisible = () => {
+    setFormVisible(!formVisible);
+  }
+
   useEffect(() =>{
     fetch('http://localhost:3000/courses')
     .then(res=>res.json())
@@ -22,6 +28,8 @@ const App =() =>{
           <CourseItem key={item.id} course={item}/>
         ))}
       </ul>
+      <button onClick={toggleFormVisible}>New course</button>
+      {formVisible && <NewCourseForm/>}
     </div>
   );
 }
---
+++ cr-frontend/src/components/NewCourseForm.tsx
@@ -0,0 +1,28 @@
+import React, {useState} from 'react';
+
+type NewCourseFormProps={
+}
+
+const NewCourseForm=(props: NewCourseFormProps)=>{
+    const [newCourseNumber,setNewCourseNumber]= useState<string>('');
+    const [newCourseTitle,setNewCourseTitle]= useState<string>('');
+    
+    const handleCourseNumberChange = (e: React.ChangeEvent<HTMLInputElement>) =>{
+        setNewCourseNumber(e.target.value);
+      }
+      
+    const handleSave =() =>{
+        alert(`${newCourseNumber} -- ${newCourseTitle}`)
+    }
+    
+   return(
+    <div>
+        Number: <input value={newCourseNumber} onChange={handleCourseNumberChange}/><br/>
+        Title: <input value={newCourseTitle} onChange={(e)=> {setNewCourseTitle(e.target.value);}}/><br/>
+        <button onClick={handleSave}>Save</button>
+    </div>
+   )
+};
+
+
+export default NewCourseForm;
